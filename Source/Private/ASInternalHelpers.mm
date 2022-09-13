ff4d3c7eac0ddccc8129fda8dd11cf34b10b0c9f
Bruce <homan311@gmail.com>
2022-09-13 21:11:01 +0800

    Fix unit test dead lock

--- Source/Private/ASInternalHelpers.mm
+++ Source/Private/ASInternalHelpers.mm
@@ -82,9 +82,9 @@ void ASInitializeFrameworkMainThreadOnDestructor(void)
     if (ASActivateExperimentalFeature(ASExperimentalLayerDefaults)) {
       // Nop. We will gather default values on-demand in ASDefaultAllowsGroupOpacity and ASDefaultAllowsEdgeAntialiasing
     } else {
-      CALayer *layer = [[[UIView alloc] init] layer];
-      allowsGroupOpacityFromUIKitOrNil = @(layer.allowsGroupOpacity);
-      allowsEdgeAntialiasingFromUIKitOrNil = @(layer.allowsEdgeAntialiasing);
+      //CALayer *layer = [[[UIView alloc] init] layer];
+      allowsGroupOpacityFromUIKitOrNil = @(YES);
+      allowsEdgeAntialiasingFromUIKitOrNil = @(NO);
     }
   });
 }
